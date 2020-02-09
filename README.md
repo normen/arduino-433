079bdd34f10ae98c6a4f2b8323d4e0ed703463ec
normen <normen667@gmail.com>
2020-02-09 02:21:18 +0100

    initial commit

---
+++ README.md
@@ -0,0 +1,92 @@
+# arduino-433
+Arduino/ESP based 433MHz home control transceiver
+
+## Introduction
+This Arduino code allows you to create a cheap 433MHz wireless transceiver to control electric switches and other home appliances. It can use USB or WiFi to connect to a computer to receive and send commands. It is mainly used for the homebridge-433-arduino plugin but can of course be used otherwise as well.
+
+The project uses the PlatformIO IDE and runs on Arduino (Micro/Nano etc.) as well as ESP8266. To decode signals rc-switch or ESPiLight can be used. Simple 433MHz receiver/sender hardware or more advanced CC1101 based transceivers are supported to receive and send 433MHz RC data.
+
+## Installation
+- Download and install the PlatformIO IDE, i.e. VisualStudioCode (see https://platformio.org)
+- Clone the project github repository, e.g. `git clone https://github.com/normen/arduino-433`
+- Open the project in PlatformIO
+- Configure the platformio.ini build options as needed (see below)
+- Configure the code as needed (see below)
+- Connect the desired microcontroller and transceiver hardware (see below)
+- Build and upload the project to your Arduino or ESP
+
+Alternatively you can copy-paste the code in `src/main.cpp` into the Arduino IDE and install the libraries listed under `lib_deps` in the `platformio.ini` file. Comment out the `#include "Arduino.h"` line when doing that.
+
+### The Hardware
+#### Microcontroller options
+- ESP8266 (3.3V) - recommended with CC1101, needed for WiFi and ESPiLight
+- Arduino Micro (5V) - recommended for the simple receivers/senders as they work best at 5V
+- Arduino Nano (3.3V) - for CC1101 without WiFi or ESPiLight
+- Others (see below)
+
+The PlatformIO project is prepared for D1 Mini (ESP8266) and Arduino Micro hardware, change `default_envs` in platformio.ini to `micro` to switch. See the PlatformIO documentation on how to compile for other boards / hardware.
+
+#### Transceiver options
+- Simple 433 receiver/sender pairs
+  - Use 5V power
+  - For the sender you can use basically any 433MHz module (e.g FS1000A)
+  - For the receiver the very cheap modules didn't work properly for me, try to get the "superheterodyne" (NOT superregeneration) receiver as it works MUCH better.
+- CC1101 based transceiver
+  - Use 3.3V power
+  - These work much better usually but might be more expensive
+
+See below for the pins used to connect the transceiver hardware.
+
+### The Software
+#### Operation
+Note: If you're using homebridge-433-arduino you can skip to the "Configuration" section unless you're interested in the interna of the transceiver communication.
+
+In its default mode the code will use the USB serial port to send rc-switch data (code, pulse, protocol) in a format like `12345/123/1` when any 433MHz codes are received and decoded. When receiving serial data in the same format the code will send the corresponding 433MHz data and return an `OK` message. The serial data is terminated by `\n`.
+
+In ESPiLight mode the code will send received signals in a JSON based format like `{"type":"switch_type","message":{"id":"A3","unit":"20","state":"off"}}`. When sending data the format changes slightly to `{"type":"switch_type","message":{"id":"A3","unit":"20","off":1}}`. See the ESPiLight documentation for more info.
+
+Accordingly, the software will only ever output 4 types of messages:
+- Message `OK` -> after receiving a message and sending the corresponding code
+- Message starting with `{` -> JSON data when using ESPiLight
+- Message starting with `pilight` -> debug data when using ESPiLight
+- Message starting with a number -> code/pulse/protocol message when using rc-switch
+
+Note that when sending RC data the software can not receive any new commands, wait until the `OK` message has been sent back before sending any new commands.
+
+#### Configuration
+To configure the code open `src/main.cpp` and change/uncomment the following `#define` parameters.
+
+##### `#define RC_INPUT_PIN D1`/ `#define RC_OUTPUT_PIN D2`
+These values are needed and represent the input (receiver) and output (transmitter) pins. The microcontroller has to support interrupts on the input pin for the receiver, almost any output pin can be used for the transmitter. Note that these are actual pin numbers, not interrupt numbers.
+
+Usual Values:
+- ESP8266 Receiver on IRQ 4 = D2
+- ESP8266 Transmit on pin 5 = D1
+- Arduino Micro Receiver on IRQ 0 = 3
+- Arduino Micro Sender on pin 4 = 4
+- Arduino Nano Receiver on IRQ 0 = 2
+- Arduino Nano Transmit = 6
+
+##### `#define USE_CC1101`
+You can use a more advanced CC1101 based transceiver module instead of simple 433MHz receiver/sender pairs. These modules also have send and receive pins but are additionally connected via SPI for configuration.
+
+See https://github.com/LSatan/RCSwitch-CC1101-Driver-Lib for interconnections to various microcontrollers.
+
+Works on Arduino and ESP.
+
+##### `#define USE_WEBSOCKET`
+You can use WiFi to connect to the computer instead of USB.
+
+This will create a websocket server to send/receive data in addition to the serial port, default hostname is `arduino-433`, port 80. Specify your WiFi credentials in `#define WIFI_SSID "ssid_here"` and `#define WIFI_PASS "pass_here"`.
+
+Works on ESP.
+
+##### `#define USE_ESPILIGHT`
+You can use ESPilight insted of rc-switch to decode switches.
+
+The send/receive data format will change to JSON, with `type` and `message` content. Additionally pilight debug messages, always beginning with `pilight` might be generated.
+
+Works on ESP.
+
+## License
+Published under the MIT License.
---
+++ src/main.cpp
@@ -0,0 +1,247 @@
+/**
+ * arduino-433 v1.0
+ * Use the arduino platform to control 433MHz switches
+ * (c) by Normen Hansen, released under MIT license
+***/
+
+// comment this out when using Arduino IDE
+#include <Arduino.h>
+
+// input pin for RC receiver -> D2 for 8266 by default, 3 for Arduino Micro by default
+#define RC_INPUT_PIN D2
+// output pin for RC transmitter -> D1 for 8266 by default, 4 for Arduino Micro by default
+#define RC_OUTPUT_PIN D1
+
+// uncomment this to use CC1101 insted of simple 443 sender/receivers
+//#define USE_CC1101
+
+// uncomment this to start websocket server for homebridge -> set WLAN login creds
+//#define USE_WEBSOCKET
+
+// uncomment this to use ESPilight insted of rc-switch to decode switches -> different message format
+//#define USE_ESPILIGHT
+
+// WIFI login if using websocket
+#ifdef USE_WEBSOCKET
+#define WIFI_SSID "my_wifi_ssid"
+#define WIFI_PASS "my_wifi_pass"
+#define WIFI_HOSTNAME "arduino-433"
+#endif
+
+// size of local message buffer
+#define CHAR_BUFFER_SIZE 255
+
+
+/**** CODE STARTS HERE ****/
+#ifdef USE_ESPILIGHT
+#include <ESPiLight.h>
+extern "C" {
+#include "pilight/libs/pilight/core/json.h"
+}
+#else
+#include <RCSwitch.h>
+#endif
+
+#ifdef USE_CC1101
+#include <ELECHOUSE_CC1101_RCS_DRV.h>
+#endif
+
+#ifdef USE_WEBSOCKET
+#ifdef ESP8266
+#include <ESP8266WiFi.h>
+#include <ESP8266WiFiMulti.h>
+#include <WebSocketsServer.h>
+#endif
+//TODO: ESP32
+#endif
+
+#ifdef USE_ESPILIGHT
+ESPiLight rf(RC_OUTPUT_PIN);  // use -1 to disable transmitter
+#else //RCSWITCH
+RCSwitch mySwitch = RCSwitch();
+#endif
+
+char receivedChars[CHAR_BUFFER_SIZE]; // an array to store the received data
+boolean newData = false; // was a full new string received?
+String dash = "/";
+
+#ifdef USE_WEBSOCKET
+ESP8266WiFiMulti WiFiMulti;
+WebSocketsServer webSocket = WebSocketsServer(80);
+uint8_t lastClientNum = 0;
+
+void copyPayload(char* src, char* dst, size_t len) {
+    for (int i = 0; i < len; i++) {
+        *dst++ = *src++;
+    }
+}
+
+void webSocketEvent(uint8_t num, WStype_t type, uint8_t * payload, size_t length) {
+  switch(type) {
+    case WStype_DISCONNECTED:
+      break;
+    case WStype_CONNECTED:
+        webSocket.sendTXT(num, "OK");
+      break;
+    case WStype_TEXT:
+      lastClientNum = num;
+      if(length <= CHAR_BUFFER_SIZE){
+        copyPayload((char*)payload, receivedChars,length);
+        newData = true;
+      }
+      break;
+    case WStype_BIN:
+      break;
+    default:
+      break;
+  }
+}
+#endif
+
+#ifdef USE_ESPILIGHT
+void espiCallback(const String &protocol, const String &message, int status,
+                size_t repeats, const String &deviceID) {
+  // check if message is valid and process it
+  if (status == VALID) {
+    String out = "{\"type\":\""+protocol+"\",\"message\":"+message+"}";
+    Serial.println(out);
+#ifdef USE_WEBSOCKET
+    webSocket.broadcastTXT(out);
+#endif
+  }
+}
+#endif
+
+void setup() {
+  Serial.begin(9600);
+  Serial.setTimeout(100);
+#ifdef USE_CC1101
+//CC1101 Settings:                (Settings with "//" are optional!)
+//ELECHOUSE_cc1101.setRxBW(16);     // set Receive filter bandwidth (default = 812khz) 1 = 58khz, 2 = 67khz, 3 = 81khz, 4 = 101khz, 5 = 116khz, 6 = 135khz, 7 = 162khz, 8 = 203khz, 9 = 232khz, 10 = 270khz, 11 = 325khz, 12 = 406khz, 13 = 464khz, 14 = 541khz, 15 = 650khz, 16 = 812khz.
+  ELECHOUSE_cc1101.setMHZ(433.92); // Here you can set your basic frequency. The lib calculates the frequency automatically (default = 433.92).The cc1101 can: 300-348 MHZ, 387-464MHZ and 779-928MHZ. Read More info from datasheet.
+  ELECHOUSE_cc1101.Init(PA10);    // must be set to initialize the cc1101! set TxPower  PA10, PA7, PA5, PA0, PA_10, PA_15, PA_20, PA_30.
+  ELECHOUSE_cc1101.SetRx();  // set Recive on
+#endif
+
+#ifdef USE_ESPILIGHT
+  rf.setCallback(espiCallback);
+  rf.initReceiver(RC_INPUT_PIN);
+#else
+  mySwitch.enableReceive(digitalPinToInterrupt(RC_INPUT_PIN));  // for esp8266! Receiver on IR 4 = D2
+  mySwitch.enableTransmit(RC_OUTPUT_PIN); // for esp8266! Transmit on pin 5 = D1
+  mySwitch.setRepeatTransmit(8);
+#endif
+
+#ifdef USE_WEBSOCKET
+  WiFi.hostname(WIFI_HOSTNAME);
+  WiFiMulti.addAP(WIFI_SSID, WIFI_PASS);
+  while(WiFiMulti.run() != WL_CONNECTED) {
+    delay(100);
+  }
+  webSocket.begin();
+  webSocket.onEvent(webSocketEvent);  
+#endif
+}
+
+// gets the values from a string formatted like 123456/123
+String getValue(String data, char separator, int index) {
+  int found = 0;
+  int strIndex[] = {0, -1};
+  int maxIndex = data.length()-1;
+  for(int i=0; i<=maxIndex && found<=index; i++){
+    if(data.charAt(i)==separator || i==maxIndex){
+      found++;
+      strIndex[0] = strIndex[1]+1;
+      strIndex[1] = (i == maxIndex) ? i+1 : i;
+    }
+  }
+  return found>index ? data.substring(strIndex[0], strIndex[1]) : "";
+}
+
+void receiveSerialData() {
+  static byte ndx = 0;
+  char endMarker = '\n';
+  char rc;   
+  if (Serial.available() > 0) {
+    rc = Serial.read();
+    if (rc != endMarker) {
+      receivedChars[ndx] = rc;
+      ndx++;
+      if (ndx >= CHAR_BUFFER_SIZE) {
+        ndx = CHAR_BUFFER_SIZE - 1;
+      }
+    }
+    else {
+      receivedChars[ndx] = '\0'; // terminate the string
+      ndx = 0;
+      newData = true;
+    }
+  }
+}
+
+void sendRcData() {
+  if (newData == true) {
+#ifdef USE_CC1101
+    ELECHOUSE_cc1101.SetTx();
+#endif
+#ifdef USE_ESPILIGHT
+    JsonNode *fullJson = json_decode(receivedChars);
+    JsonNode *typeJson = json_find_member(fullJson, "type");
+    JsonNode *messageJson = json_find_member(fullJson, "message");
+    char *message = json_encode(messageJson);
+    rf.send(typeJson->string_, message);
+    json_free(message);
+    json_delete(messageJson);
+    json_delete(typeJson);
+    json_delete(fullJson);
+#else
+    long value = getValue(receivedChars, '/', 0).toInt();
+    long pulse = getValue(receivedChars, '/', 1).toInt();
+    long protocol = getValue(receivedChars, '/', 2).toInt();
+    if(protocol==0) protocol = 1;
+    mySwitch.setProtocol(protocol);
+    mySwitch.setPulseLength(pulse);
+    mySwitch.send(value, 24);
+#endif
+#ifdef USE_CC1101
+    ELECHOUSE_cc1101.SetRx();
+#endif
+#ifdef USE_WEBSOCKET
+    webSocket.sendTXT(lastClientNum, "OK");
+#endif
+    Serial.println("OK");
+    newData = false;
+  }
+}
+
+#ifdef USE_ESPILIGHT
+#else
+void receiveRcData(){
+  if (mySwitch.available()) {
+    long value = mySwitch.getReceivedValue();
+    long pulse = mySwitch.getReceivedDelay();
+    long protocol = mySwitch.getReceivedProtocol();
+    if (value != 0) {
+      String out = value + dash + pulse + dash + protocol;
+#ifdef USE_WEBSOCKET
+      webSocket.broadcastTXT(out);
+#endif
+      Serial.println( out );
+    }
+    mySwitch.resetAvailable();
+  }  
+}
+#endif
+
+void loop() {
+#ifdef USE_WEBSOCKET
+  webSocket.loop();
+#endif
+  receiveSerialData();
+  sendRcData();
+#ifdef USE_ESPILIGHT
+  rf.loop();
+#else
+  receiveRcData();
+#endif
+}
