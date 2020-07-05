ef73291a841496245ad0e3eb79436affe45c58b3
normen <normen667@gmail.com>
2020-07-05 20:41:22 +0200

    v1.2 - fix initialization order for latest CC1101 lib

--- src/main.cpp
+++ src/main.cpp
@@ -1,5 +1,5 @@
 /**
- * arduino-433 v1.1
+ * arduino-433 v1.2
  * Use the arduino platform to control 433MHz switches
  * (c) by Normen Hansen, released under MIT license
 ***/
@@ -124,9 +124,9 @@ void setup() {
   Serial.setTimeout(100);
 #ifdef USE_CC1101
 //CC1101 Settings:                (Settings with "//" are optional!)
+  ELECHOUSE_cc1101.Init();    // must be set to initialize the cc1101! set TxPower  PA10, PA7, PA5, PA0, PA_10, PA_15, PA_20, PA_30.
 //ELECHOUSE_cc1101.setRxBW(16);     // set Receive filter bandwidth (default = 812khz) 1 = 58khz, 2 = 67khz, 3 = 81khz, 4 = 101khz, 5 = 116khz, 6 = 135khz, 7 = 162khz, 8 = 203khz, 9 = 232khz, 10 = 270khz, 11 = 325khz, 12 = 406khz, 13 = 464khz, 14 = 541khz, 15 = 650khz, 16 = 812khz.
   ELECHOUSE_cc1101.setMHZ(433.92); // Here you can set your basic frequency. The lib calculates the frequency automatically (default = 433.92).The cc1101 can: 300-348 MHZ, 387-464MHZ and 779-928MHZ. Read More info from datasheet.
-  ELECHOUSE_cc1101.Init();    // must be set to initialize the cc1101! set TxPower  PA10, PA7, PA5, PA0, PA_10, PA_15, PA_20, PA_30.
   ELECHOUSE_cc1101.SetRx();  // set Recive on
 #endif
 
